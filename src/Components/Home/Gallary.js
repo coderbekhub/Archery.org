429519fe9639c5bd1eea2c9b0ee1896118e1e2c7
coderbekhub <coderbek@mail.ru>
2024-06-24 16:21:08 +0500

    ..

--- src/Components/Home/Gallary.js
+++ src/Components/Home/Gallary.js
@@ -10,12 +10,7 @@ import GalleryImg2 from '../../img/atletsNews/atletNews2.jpg'
 import GalleryImg3 from '../../img/atletsNews/atletNews3.jpg'
 import GalleryImg4 from '../../img/atletsNews/atletNews4.jpg'
 import GalleryImg5 from '../../img/atletsNews/atletNews5.jpg'
-import GalleryImg6 from '../../img/atletsNews/atletNews6.jpg'
-import GalleryImg7 from '../../img/atletsNews/atletNews7.jpg'
-import GalleryImg8 from '../../img/atletsNews/atletNews8.jpg'
-import GalleryImg9 from '../../img/atletsNews/atletNews9.jpg'
 import GalleryImg10 from '../../img/atletsNews/atletNews10.jpg'
-import GalleryImg11 from '../../img/atletsNews/atletNews11.jpg'
 
 export default function Gallary() {
   return (
@@ -29,15 +24,15 @@ export default function Gallary() {
               <img className='galleryBigImg' src={GalleryImg10} alt="..." />
             </div>
             <div className='gallaryInfo2 col-md-6 col-6'>
-              <img src={GalleryImg2} alt="..." width={350} height={264}/>
-              <img src={GalleryImg1} alt="..." width={350} height={264}/>
+              <img src={GalleryImg2} alt="..." />
+              <img src={GalleryImg1} alt="..." />
             </div>
           </div>
 
           <div className='gallaryInfos col-md-4 col-12'>
-            <img src={GalleryImg3} alt="..."  width={350} height={264} />
-            <img src={GalleryImg4} alt="..."  width={350} height={264} />
-            <img src={GalleryImg5} alt="..."  width={350} height={264} />
+            <img src={GalleryImg3} alt="..." />
+            <img src={GalleryImg4} alt="..." />
+            <img src={GalleryImg5} alt="..." />
           </div>
         </div>
       </div>
